a4411f04140cea3a247b6f740258b8bc2777ede9
GabrielHerrera-1 <gabrielherreraufsc@gmail.com>
2024-10-22 11:41:59 -0300

    Update LivreMercado.java

--- LivreMercado.java
+++ LivreMercado.java
@@ -10,38 +10,67 @@ public class LivreMercado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Categoria cat1 = new Categoria("cat1");
-        Categoria cat2 = new Categoria("cat2");
-        Categoria cat3 = new Categoria("cat3");
-        Categoria cat4 = new Categoria("cat4");
+        Categoria cat1 = new Categoria("categoria 1");
+        Categoria cat2 = new Categoria("categoria 2");
+        Categoria cat3 = new Categoria("categoria 3");
+        Categoria cat4 = new Categoria("categoria 4");
         
         cat1.adicioneSubCategoria(cat3);
         cat3.adicioneSubCategoria(cat4);
         cat3.adicioneSubCategoria(cat2);
-        //cat1 tem a cat3 que tem (cat4 e cat2)
-       
-        Produto prod1 = new Produto("prod1", "2", "3",2, cat1);
-        Produto prod2 = new Produto("prod2", "2", "3",2, cat2);
-        Produto prod3 = new Produto("prod3", "2", "3",2, cat3);
-        Produto prod4 = new Produto("prod4", "2", "3",2, cat4);
         
-        System.out.println("cat1 prods:");
+        Produto prod1 = new Produto("Produto 1", "2", "3",2, cat1);
+        Produto prod2 = new Produto("Produto 2", "2", "3",2, cat2);
+        Produto prod3 = new Produto("Produto 3", "2", "3",2, cat3);
+        Produto prod4 = new Produto("Produto 4", "2", "3",2, cat4);
+        
+        cat1.adicioneProduto(prod1);
+        cat2.adicioneProduto(prod2);
+        cat3.adicioneProduto(prod3);
+        cat4.adicioneProduto(prod4);
+        
+        System.out.println("categoria 1 produtos:");
         cat1.getProdutos().forEach(p -> System.out.println(p.getNome() + " " + p.getCategoria().getNome()));
-        System.out.println("cat2 prods:");
+        
+        System.out.println("categoria 2 produtos:");
         cat2.getProdutos().forEach(p -> System.out.println(p.getNome() + " " + p.getCategoria().getNome()));
-        System.out.println("cat3 prods:");
+        
+        System.out.println("categoria 3 produtos:");
         cat3.getProdutos().forEach(p -> System.out.println(p.getNome() + " " + p.getCategoria().getNome()));
-        System.out.println("cat4 prods:");
+        
+        System.out.println("categoria 4 produtos:");
         cat4.getProdutos().forEach(p -> System.out.println(p.getNome() + " " + p.getCategoria().getNome()));
         
+        System.out.println("Subcategorias de cada categoria:");
+        
+        System.out.println("categoria 1 subcategorias:");
+        cat1.getSubcategorias().forEach(p -> System.out.println(p.getNome()));
+        System.out.println("categoria 2 subcategorias:");
+        cat2.getSubcategorias().forEach(p -> System.out.println(p.getNome()));
+        System.out.println("categoria 3 subcategorias:");
+        cat3.getSubcategorias().forEach(p -> System.out.println(p.getNome()));
+        System.out.println("categoria 4 subcategorias:");
+        cat4.getSubcategorias().forEach(p -> System.out.println(p.getNome()));
+        
         cat1.removaSubcategoria(cat3, true);
-        // remover cat 3 da 1, deve deixar a cat4 e 2 na cat 1, além dos produtos prod 1
         System.out.println("Removendo subcategoria 3 da 1...");
         
-        System.out.println("cat3 prods:");
+        System.out.println("categoria 3 produtos:");
         cat3.getProdutos().forEach(p -> System.out.println(p.getNome() + " " + p.getCategoria().getNome()));
-        System.out.println("cat1 prods:");
+        System.out.println("categoria 1 produtos:");
         cat1.getProdutos().forEach(p -> System.out.println(p.getNome() + " " + p.getCategoria().getNome()));
-    }
+        
+        
+        System.out.println("Subcategorias de cada categoria:");
+        
+        System.out.println("categoria 1 subcategorias:");
+        cat1.getSubcategorias().forEach(p -> System.out.println(p.getNome()));
+        System.out.println("categoria 2 subcategorias:");
+        cat2.getSubcategorias().forEach(p -> System.out.println(p.getNome()));
+        System.out.println("categoria 3 subcategorias:");
+        cat3.getSubcategorias().forEach(p -> System.out.println(p.getNome()));
+        System.out.println("categoria 4 subcategorias:");
+        cat4.getSubcategorias().forEach(p -> System.out.println(p.getNome()));
+        }
     
 }
